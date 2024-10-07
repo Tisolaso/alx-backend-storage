f5acd2c0456c680555e3daf9d69918a3f3e8aba5
Youssef <spanya2017@gmail.com>
2024-10-07 18:42:46 +0100

    lol

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -6,4 +6,4 @@
 def insert_school(mongo_collection, **kwargs):
     "Inser a new Document"
 
-    return mongo_collection.insert_one(kwargs)._id
+    return mongo_collection.insert_one(kwargs).inserted_id
